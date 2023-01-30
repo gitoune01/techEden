06a84bba4a7482c5748a8b01195c921db38490ba
gitoune01 <docgyno@gmail.com>
2023-01-30 22:16:28 +0100

    navbar created and setup react router

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,24 @@
-import './App.css';
+import {ChakraProvider, chakraProvider} from '@chakra-ui/react'
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import Navbar from './components/Navbar';
+import Cart from './screens/Cart';
+import Products from './screens/Products';
 
 function App() {
   return (
-    <h2>Hello World!</h2>  
-    
+     <ChakraProvider>
+       <Router>
+          <Navbar />
+           <main>
+             <Routes>
+               <Route path="/products" element={<Products />} />
+               <Route path="/cart" element={<Cart />} />
+
+
+             </Routes>
+           </main>
+       </Router>
+    </ChakraProvider>
   );
 }
 
